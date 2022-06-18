d8a44ef362fd0967b280d4a585bf41dac0db8b2c
Paul Franklin <paul@ubuntu.attlocal.net>
2022-06-18 06:46:21 +0000

    documentation for test design

--- test/java/pf/pfranklin0/insertion_sort/InsertionSortTest.java
+++ test/java/pf/pfranklin0/insertion_sort/InsertionSortTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests will be written for the InsertionSort class.
+ * 
+ * 3A is a test design pattern that is usually studied by developers. Each part of 3A has a different responsibility, which can be identified at:
+ * https://java-design-patterns.com/patterns/arrange-act-assert/
+ * 
  */
 public class InsertionSortTest 
 {
